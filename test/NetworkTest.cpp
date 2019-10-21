819a42078593b9b2a4672f54fe8b5fb2de58e3fe
Arjung27 <arjun.g1511@gmail.com>
2019-10-21 07:49:30 -0400

    [NetworkTest.cpp][Task 2.5]Unit tests for DetectionModule class

--- test/NetworkTest.cpp
+++ test/NetworkTest.cpp
@@ -31,3 +31,43 @@
  */
 
 #include <gtest/gtest.h>
+
+#include <Network.hpp>
+
+/**
+ * @brief Test to check blob creation
+ *
+ * @param none
+ *
+ * @return none
+ */
+TEST(NetworkTest, TestCreateNetworkInput) {
+
+  Network network;
+
+  cv::Mat testImage1 = cv::imread("../test/testData/testImage.jpg");
+  cv::Mat testImage2 = cv::imread("../test/testData/notTestImage.jpg");
+  int testFlag1 = network.createNetworkInput(testImage1);
+  int testFlag2 = network.createNetworkInput(testImage2);
+
+  ASSERT_EQ(1, testFlag1);
+  ASSERT_EQ(0, testFlag2);
+}
+
+/**
+ * @brief Test to check network execution
+ *
+ * @param none
+ *
+ * @return none
+ */
+TEST(NetworkTest, TestApplyYOLONetwork) {
+
+  Network network;
+
+  cv::Mat testImage = cv::imread("../test/testData/testImage.jpg");
+  int testFlag = network.createNetworkInput(testImage);
+  std::vector<cv::Mat> testDetections = network.applyYOLONetwork();
+
+  ASSERT_GE(testDetections.size(), 1);
+}
